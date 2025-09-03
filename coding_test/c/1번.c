13156464f802ccce5aab769ca2e254c7597fa394
LeeHoHyun <gahohyun04@naver.com>
2025-09-03 13:56:37 +0900

    First commit

---
+++ coding_test/c/1번.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("나는 이호현입니다.");
+}
---
+++ coding_test/python/1번.py
@@ -0,0 +1,7 @@
+print("나는 이호현입니다")
+
+def print_name():
+    print("나는 ", name, "입니다")
+
+name = input("이름을 입력하세요: ")
+print_name()
